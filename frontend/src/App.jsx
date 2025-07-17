1bdf3efe6051cad73669055fa0c869920d1b0e86
Chinmay1635 <chinmaykulkarni165@gmail.com>
2025-07-17 22:42:42 +0530

    added basic prize cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Roadmap from './components/Roadmap'
 import TechnologiesSection from './components/TechnologiesSection'
+import PrizeSection from './components/PrizeSection'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       {/* <RoadmapSection/> */}
       <Roadmap/>
       <TechnologiesSection/>
+      <PrizeSection/>
       <Footer/>
     </div>
   )
