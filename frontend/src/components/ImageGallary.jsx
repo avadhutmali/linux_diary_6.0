9a78a08998aabe151648f7c3279c7d10c66ef982
Chinmay1635 <chinmaykulkarni165@gmail.com>
2025-07-24 20:09:12 +0530

    Added images path for gallary

--- frontend/src/components/ImageGallary.jsx
+++ frontend/src/components/ImageGallary.jsx
@@ -1,36 +1,81 @@
 import React from "react";
 import InfiniteMenu from "./InfiniteMenu";
+import Img1 from "../assets/diary/Img1.jpg";
+import Img2 from "../assets/diary/Img2.jpg";
+import Img3 from "../assets/diary/Img3.jpg";
+import Img4 from "../assets/diary/Img4.jpg";
+import Img5 from "../assets/diary/Img5.jpg";
+import Img6 from "../assets/diary/Img6.jpg";
+import Img7 from "../assets/diary/Img7.jpg";
+import Img8 from "../assets/diary/Img8.jpg";
+import Img9 from "../assets/diary/Img9.jpg";
+import Img10 from "../assets/diary/Img10.jpg";
+import Img11 from "../assets/diary/Img11.jpg";
+import Img12 from "../assets/diary/Img12.jpg";
+
+
 
 const items = [
   {
-    image: "https://picsum.photos/300/300?grayscale",
+    image: Img1,
     link: "https://google.com/",
     title: "Item 1",
     description: "This is pretty cool, right?",
   },
   {
-    image: "https://picsum.photos/400/400?grayscale",
+    image: Img2,
     link: "https://google.com/",
     title: "Item 2",
     description: "This is pretty cool, right?",
   },
   {
-    image: "https://picsum.photos/500/500?grayscale",
+    image: Img3,
     link: "https://google.com/",
     title: "Item 3",
     description: "This is pretty cool, right?",
   },
   {
-    image: "https://picsum.photos/600/600?grayscale",
+    image: Img4,
     link: "https://google.com/",
     title: "Item 4",
     description: "This is pretty cool, right?",
   },
+    {
+    image: Img5,
+    link: "https://google.com/",
+    title: "Item 4",
+    description: "This is pretty cool, right?",
+  },
+  {
+    image: Img6,
+    link: "https://google.com/",
+    title: "Item 4",
+    description: "This is pretty cool, right?",
+  },  
+  {
+    image: Img7,
+    link: "https://google.com/",
+    title: "Item 4",
+    description: "This is pretty cool, right?",
+  },
+    {
+    image: Img8,
+    link: "https://google.com/",
+    title: "Item 4",
+    description: "This is pretty cool, right?",
+  },
+    {
+    image: Img9,
+    link: "https://google.com/",
+    title: "Item 4",
+    description: "This is pretty cool, right?",
+  },
+
 ];
 
 const ImageGallary = () => {
   return (
-    <div style={{ height: "600px", position: "relative" }}>
+    <div style={{ height: "80vh", position: "relative" }}>
       <InfiniteMenu items={items} />
     </div>
   );
